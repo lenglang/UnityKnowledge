e23340e251768e4316d41ba770f7198ba53010c8
lenglang <364127728@qq.com>
2017-01-20 15:01:29 +0800

    版本提交

---
+++ Editor/AudioWindowEditor.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+/// <summary>
+/// 音效管理面板
+/// </summary>
+public class AudioWindowEditor : EditorWindow {
+    [MenuItem("自定义窗口/AudioManager")]
+    static void CreateWindow()
+    {
+        //Rect rect = new Rect(400, 400, 300, 400);
+        //AudioWindowEditor window = EditorWindow.GetWindowWithRect(typeof(AudioWindowEditor), rect) as AudioWindowEditor;
+        AudioWindowEditor window = EditorWindow.GetWindow<AudioWindowEditor>("音效管理");
+        window.Show();
+    }
+
+    private string audioName;
+    private string audioPath;
+    private Dictionary<string, string> audioDict = new Dictionary<string, string>();
+
+    void Awake()
+    {
+        //Debug.Log(EditorApplication.applicationPath);
+        //Debug.Log(EditorApplication.applicationContentsPath);
+        //Debug.Log(Application.dataPath);
+        LoadAudioList();
+    }
+    void OnGUI()
+    {
+        //EditorGUILayout.TextField("输入文字1", text);
+        //GUILayout.TextField("输入文字2");
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("音效名称");
+        GUILayout.Label("音效路径");
+        GUILayout.Label("操作");
+        GUILayout.EndHorizontal();
+        foreach (string key in audioDict.Keys)
+        {
+            string value;
+            audioDict.TryGetValue(key, out value);
+            GUILayout.BeginHorizontal();
+            GUILayout.Label(key);
+            GUILayout.Label(value);
+            if (GUILayout.Button("删除"))
+            {
+                audioDict.Remove(key);
+                SaveAudioList();
+                return;
+            }
+            GUILayout.EndHorizontal();
+        }
+
+        audioName = EditorGUILayout.TextField("音效名字", audioName);
+        audioPath = EditorGUILayout.TextField("音效路径", audioPath);
+        if (GUILayout.Button("添加音效")) 
+        { 
+            object o = Resources.Load(audioPath);
+            if (o == null)
+            {
+                Debug.LogWarning("音效不存在于" + audioPath + " 添加不成功");
+                audioPath = "";
+            }
+            else {
+                if (audioDict.ContainsKey(audioName))
+                {
+                    Debug.LogWarning("名字已经存在，请修改");
+                }
+                else
+                {
+                    audioDict.Add(audioName, audioPath);
+                    SaveAudioList();
+                }
+            }
+        }
+    }
+
+    //窗口面板被更新的时候调用
+    void OnInspectorUpdate()
+    {
+        //Debug.Log("Update");
+        LoadAudioList();
+    }
+
+    
+
+    private void SaveAudioList()
+    {
+        StringBuilder sb = new StringBuilder();
+         
+        foreach (string key in audioDict.Keys)
+        {
+            string value;
+            audioDict.TryGetValue(key, out value);
+            sb.Append(key + "," + value + "\n");
+        } 
+        File.WriteAllText(AudioManager.AudioTextPath, sb.ToString());
+        //File.AppendAllText(savePath, sb.ToString());
+    }
+    private void LoadAudioList()
+    {
+        audioDict = new Dictionary<string, string>();
+        if (File.Exists(AudioManager.AudioTextPath) == false) return;
+        string[] lines = File.ReadAllLines(AudioManager.AudioTextPath);
+        foreach (string line in lines)
+        {
+            if (string.IsNullOrEmpty(line)) continue;
+            string[] keyvalue = line.Split(',');
+            audioDict.Add(keyvalue[0], keyvalue[1]);
+        }
+    }
+}
---
+++ Editor/ExcelCSVToJson.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System.IO;
+using System.Text;
+using SimpleJSON;
+
+public class ExcelToJson{
+
+	[MenuItem("小工具/ExcelCSVToJson")]
+	static void excelToJson()
+	{
+		string dataFolderPath=Application.dataPath+"/WZK/Scripts/ExcelToJson/Data";
+        string outJsonPath = Application.dataPath + "/WZK/Scripts/ExcelToJson/Json";
+		if(!Directory.Exists(dataFolderPath))
+		{
+			Debug.LogError("请建立"+dataFolderPath+" 文件夹，并且把csv文件放入此文件夹内");
+			return;
+		}
+		string[] allCSVFiles=Directory.GetFiles(dataFolderPath,"*.csv");
+		if(allCSVFiles==null||allCSVFiles.Length<=0)
+		{
+			Debug.LogError(""+dataFolderPath+" 文件夹没有csv文件,请放入csv文件到此文件夹内");
+			return;
+		}
+		if(!Directory.Exists(outJsonPath))
+		{
+			Directory.CreateDirectory(outJsonPath);
+		}
+
+		for(int i=0;i<allCSVFiles.Length;i++)
+		{
+			string dictName=new DirectoryInfo(Path.GetDirectoryName(allCSVFiles[i])).Name;
+			string fileName=Path.GetFileNameWithoutExtension(allCSVFiles[i]);
+
+			string jsonData=readExcelData(allCSVFiles[i]);
+			outJsonContentToFile(jsonData,outJsonPath+"/"+dictName+"/"+fileName+".json");
+		}
+
+	}
+	static string readExcelData(string fileName)
+	{
+		if(!File.Exists(fileName))
+		{
+			return null;
+		}
+		string fileContent=File.ReadAllText(fileName,UnicodeEncoding.Default);
+		string[] fileLineContent=fileContent.Split(new string[]{"\r\n"},System.StringSplitOptions.None);
+		if(fileLineContent!=null)
+		{
+			//注释的名字
+			//string[] noteContents=fileLineContent[0].Split(new string[]{","},System.StringSplitOptions.None);
+			//变量的名字
+			string[] VariableNameContents=fileLineContent[1].Split(new string[]{","},System.StringSplitOptions.None);
+
+			JSONClass jsonData=new JSONClass();
+			for(int i=2;i<fileLineContent.Length-1;i++)
+			{
+				string[] lineContents=fileLineContent[i].Split(new string[]{","},System.StringSplitOptions.None);
+				JSONClass classLine=new JSONClass();
+				for(int j=1;j<lineContents.Length;j++)
+				{
+					classLine[VariableNameContents[j]]=lineContents[j];
+				}
+				jsonData[lineContents[0]]=classLine;
+			}
+			string resultJson=jsonData.ToString("");
+			return resultJson;
+		}
+		return null;
+	}
+
+	static void outJsonContentToFile(string jsonData,string jsonFilePath)
+	{
+		string directName=Path.GetDirectoryName(jsonFilePath);
+		if(!Directory.Exists(directName))
+		{
+			Directory.CreateDirectory(directName);
+		}
+		File.WriteAllText(jsonFilePath,jsonData,Encoding.UTF8);
+		Debug.Log("成功输出Json文件  :"+jsonFilePath);
+	}
+
+}

88cce2f54acde500df62e23490b7b80871f283c0
lenglang <364127728@qq.com>
2017-03-06 15:55:49 +0800

    2017-3-6

---
+++ Scripts/Protobufnet/ProtobufTool.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+using System.Collections;
+public class ProtobufTool{
+    // 将消息序列化为二进制的方法 
+    // < param name="model">要序列化的对象< /param> 
+    public static byte[] Serialize<T>(T model)
+    {
+        try
+        {
+            //涉及格式转换，需要用到流，将二进制序列化到流中    
+            using (MemoryStream ms = new MemoryStream())
+            {
+                //使用ProtoBuf工具的序列化方法    
+                ProtoBuf.Serializer.Serialize<T>(ms, model);
+                //定义二级制数组，保存序列化后的结果     
+                byte[] result = new byte[ms.Length];
+                //将流的位置设为0，起始点    
+                ms.Position = 0;
+                //将流中的内容读取到二进制数组中    
+                ms.Read(result, 0, result.Length);
+                return result;
+            }
+        }
+        catch (Exception ex) { Debug.LogError("序列化失败: " + ex.ToString()); return null; }
+    }
+    // 将收到的消息反序列化成对象 
+    // < returns>The serialize.< /returns> 
+    // < param name="msg">收到的消息.</param> 
+    public static T DeSerialize<T>(byte[] msg)
+    {
+        try
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                //将消息写入流中     
+                ms.Write(msg, 0, msg.Length);
+                //将流的位置归0    
+                ms.Position = 0;
+                //使用工具反序列化对象     
+                T result = ProtoBuf.Serializer.Deserialize<T>(ms);
+                return result;
+            }
+        }
+        catch (Exception ex) { Debug.LogError("反序列化失败: " + ex.ToString()); return default(T); }
+    }
+
+}
--- Scripts/Protobufnet/TestProtobuf.cs
+++ Scripts/Protobufnet/TestProtobuf.cs
@@ -3,10 +3,12 @@ using System.Collections;
 using ProtoBuf;
 using System.IO;
 
-public class TestProtobuf : MonoBehaviour {
+public class TestProtobuf : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         string dataPath = Application.dataPath + "/UnityKnowledge/Scripts/Protobufnet/user.bin";
         if (File.Exists(dataPath) == false)
         {
@@ -25,7 +27,7 @@ public class TestProtobuf : MonoBehaviour {
                 Serializer.Serialize<User>(fs, user);
             }
         }
-        
+
 
         User nuser = null;
 
@@ -38,10 +40,60 @@ public class TestProtobuf : MonoBehaviour {
         print(nuser.Username);
         print(nuser.Password);
         print(nuser.Level);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    }
+    /// <summary>
+    /// 将文件内容转成数据流
+    /// </summary>
+    /// <param name="fileName"></param>
+    /// <returns></returns>
+    private byte[] ReadFile(string fileName)
+    {
+        FileStream pFileStream = null;
+        byte[] pReadByte = new byte[0];
+        try
+        {
+            pFileStream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            BinaryReader r = new BinaryReader(pFileStream);
+            r.BaseStream.Seek(0, SeekOrigin.Begin);    //将文件指针设置到文件开  
+            pReadByte = r.ReadBytes((int)r.BaseStream.Length);
+            return pReadByte;
+        }
+        catch
+        {
+            return pReadByte;
+        }
+
+        finally
+        {
+            if (pFileStream != null)
+                pFileStream.Close();
+        }
+    }
+    /// <summary>
+    /// 数据流写入文件
+    /// </summary>
+    /// <param name="pReadByte"></param>
+    /// <param name="fileName"></param>
+    /// <returns></returns>
+    private bool writeFile(byte[] pReadByte, string fileName)
+    {
+
+        FileStream pFileStream = null;
+        try
+        {
+            pFileStream = new FileStream(fileName, FileMode.OpenOrCreate);
+            pFileStream.Write(pReadByte, 0, pReadByte.Length);
+        }
+        catch
+        {
+            return false;
+        }
+        finally
+        {
+            if (pFileStream != null)
+                pFileStream.Close();
+        }
+        return true;
+    }  
 }

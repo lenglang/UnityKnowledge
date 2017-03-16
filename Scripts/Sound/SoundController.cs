a25cf0592586d4a96696691723eaf5cb78a7d7d8
lenglang <364127728@qq.com>
2017-03-16 13:26:57 +0800

    2017-3-16

--- Scripts/Sound/SoundController.cs
+++ Scripts/Sound/SoundController.cs
@@ -1,47 +1,85 @@
-﻿
+﻿using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
-public enum VoiceType
-    {
-        把相同的东西找出来吧,
-    }
+using System.Linq;
     public enum SoundType
     {
-        出场可爱的嚎叫的声音,
+        熊猫完成转身第一脚落下时播放,
+        点击拿起衣服时,
+        衣服落入洗衣篮时
+    }
+    public enum VoiceType
+    {
+        阳光真好我们来洗衣服吧,
+        白色和彩色的衣服要分开放哦,
+        就是这样
     }
     public enum SoundID
     {
         背景,
         语音,
-        音效,
+        音效
     }
-    public class SoundController : Singleton<SoundController>
+    public class SoundControl : Singleton<SoundControl>
     {
-        public string BackGoundSound = "春节背景";
-        public float SoundVolume = 0.6f;
-        public void IsPlayBackGroundSound(bool isPlay)
+        [Header("背景音乐")]
+        public AudioClip _bgAudioClip;
+        [Range(0, 1)]
+        public float _bgSoundVolume = 0.6f;
+        [Header("音效列表")]
+        public List<AudioClip> _soundList = new List<AudioClip>();
+        [Header("人声列表")]
+        public List<AudioClip> _voiceList = new List<AudioClip>();
+        private Dictionary<string, AudioClip> _soundDictionary = new Dictionary<string, AudioClip>();
+        private Dictionary<string, AudioClip> _voiceDictionary = new Dictionary<string, AudioClip>();
+        protected override void Awake()
         {
-            //if (isPlay)
-            // PlaySound(BackGoundSound, false).SetLoop(true).SetID(GetSoundID(SoundID.背景)).SetVolume(0.6f);
-            //else
-            //DestroySound(BackGoundSound, false);
+            base.Awake();
+            foreach (AudioClip item in _soundList)
+            {
+                _soundDictionary.Add(item.name, item);
+            }
+            foreach (AudioClip item in _voiceList)
+            {
+                _voiceDictionary.Add(item.name, item);
+            }
+
+        }
+        public void IsPlayBgSound(bool isPlay)
+        {
+            if (isPlay)
+            {
+                PlaySound(_bgAudioClip, "背景音乐", false).SetLoop(true).SetID(GetSoundID(SoundID.背景)).SetVolume(_bgSoundVolume);
+            }
+            else { DestroySound("背景音乐", false); }
         }
         string GetSoundName(VoiceType type)
         {
             switch (type)
             {
-                case VoiceType.把相同的东西找出来吧:
-                    return "01001把相同的东西找出来吧~";
+                case VoiceType.阳光真好我们来洗衣服吧:
+                    return "1";
+                case VoiceType.白色和彩色的衣服要分开放哦:
+                    return "2";
+                case VoiceType.就是这样:
+                    return "3";
+                default:
+                    return "";
             }
-            return "";
         }
         string GetSoundName(SoundType type)
         {
             switch (type)
             {
-                case SoundType.出场可爱的嚎叫的声音:
-                    return "出场可爱的嚎叫的声音";
+                case SoundType.熊猫完成转身第一脚落下时播放:
+                    return "01001";
+                case SoundType.点击拿起衣服时:
+                    return "01002";
+                case SoundType.衣服落入洗衣篮时:
+                    return "01003";
+                default:
+                    return "";
             }
-            return "";
         }
         public string GetSoundID(SoundID id)
         {
@@ -57,31 +95,43 @@ public enum VoiceType
                     return "";
             }
         }
-        public Sound Play(VoiceType type, bool isRepeat = true)
+        public float GetSoundLength(VoiceType type)
         {
             string soundName = GetSoundName(type);
+            return _voiceDictionary[soundName].length;
+        }
+        public float GetSoundLength(SoundType type)
+        {
+            string soundName = GetSoundName(type);
+            return _soundDictionary[soundName].length;
+        }
+        public Sound Play(VoiceType type, bool isRepeat = true)
+        {
+            //不叠加播放
+            DestroyAllSound(SoundID.语音);
+            string soundName = GetSoundName(type);
             if (soundName.Equals("")) return null;
-
-            Sound newSound = PlaySound(soundName, isRepeat).SetVolume(0.8f);
+            Sound newSound = PlaySound(_voiceDictionary[soundName], soundName, isRepeat);
             if (newSound != null)
                 newSound.SetID(GetSoundID(SoundID.语音));
             return newSound;
         }
         public Sound Play(SoundType type, bool isRepeat = true)
         {
+            Debug.Log(type);
             string soundName = GetSoundName(type);
             if (soundName.Equals("")) return null;
-            Sound sound = PlaySound(soundName, isRepeat);
+            Sound sound = PlaySound(_soundDictionary[soundName], soundName, isRepeat);
             if (sound != null)
                 sound.SetID(GetSoundID(SoundID.音效));
             return sound;
         }
-        Sound PlaySound(string soundName, bool isRepeat)
+        Sound PlaySound(AudioClip clip, string soundName, bool isRepeat)
         {
             if (!isRepeat)
                 if (IsPlaying(soundName))
                     return null;
-            return SoundManager.Instance.PlayFromAssetbundle(soundName);
+            return SoundManager.Instance.PlaySound(clip, soundName);
         }
         public bool IsPlaying(SoundType type)
         {
@@ -97,7 +147,7 @@ public enum VoiceType
                 return true;
             return false;
         }
-        bool IsPlaying(string soundName)
+        public bool IsPlaying(string soundName)
         {
             if (GetSound(soundName) != null)
                 return true;
@@ -107,12 +157,11 @@ public enum VoiceType
         {
             return SoundManager.Instance.SearchSound(soundName);
         }
-
         Sound GetSound(string soundName)
         {
             List<Sound> sounds = GetSounds(soundName);
             if (sounds.Count != 0)
-                return sounds[0];
+                return sounds.Last();
             else
                 return null;
         }
@@ -131,7 +180,7 @@ public enum VoiceType
             string soundName = GetSoundName(type);
             List<Sound> sounds = GetSounds(soundName);
             if (sounds.Count != 0)
-                return sounds[0];
+                return sounds.Last();
             else
                 return null;
         }
@@ -140,7 +189,7 @@ public enum VoiceType
             string soundName = GetSoundName(type);
             List<Sound> sounds = GetSounds(soundName);
             if (sounds.Count != 0)
-                return sounds[0];
+                return sounds.Last();
             else
                 return null;
         }

8fca1eb0deb633fa87e91f6902ecedb6a05d343a
王则昆 <364127728@qq.com>
2017-06-27 09:27:46 +0800

    歌词播放优化

--- Scripts/Lyrics/Test.cs
+++ Scripts/Lyrics/Test.cs
@@ -11,12 +11,16 @@ using DG.Tweening;
 public class Test : MonoBehaviour
 {
     public Transform _canvastf;
-    public Transform _text;
-    public float _countTime = 0;
+    public Text _text;
+    public RectTransform _imageRtf;
+    [HideInInspector]
     public int _frame = 24;//帧频
+    private float _countTime = 0;
     void Start ()
     {
-        RectTransform rtf = this.GetComponent<RectTransform>();
+        //获取该文本的宽高
+        //_text.preferredWidth
+        //_text.preferredHeight
         _countTime = Time.time;
         float number = 0;
         DOTween.To(() => number, x => number = x, 1000, 10f).OnUpdate(delegate
@@ -25,8 +29,8 @@ public class Test : MonoBehaviour
             {
                 _countTime = Time.time;
                 _text.transform.SetParent(_canvastf);
-                rtf.sizeDelta = new Vector2(number, 80);
-                _text.transform.SetParent(this.transform);
+                _imageRtf.sizeDelta = new Vector2(number, 80);
+                _text.transform.SetParent(_imageRtf);
             }
 
         }).SetEase(Ease.Linear).SetLoops(-1,LoopType.Yoyo);

788573fce49987146cbe91ee993f2a98e98c213d
王则昆 <364127728@qq.com>
2017-07-03 11:35:54 +0800

    导航更新

---
+++ 导航/Scripts/NavigationExample.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NavigationExample : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        
+	}
+    /// <summary>
+    /// 获取导航随机点
+    /// </summary>
+    /// <returns></returns>
+    public Vector3 GetRandomLocation()
+    {
+        NavMeshTriangulation navMeshData = NavMesh.CalculateTriangulation();
+        int t = Random.Range(0, navMeshData.indices.Length - 3);
+        Vector3 point = Vector3.Lerp(navMeshData.vertices[navMeshData.indices[t]], navMeshData.vertices[navMeshData.indices[t + 1]], Random.value);
+        point = Vector3.Lerp(point, navMeshData.vertices[navMeshData.indices[t + 2]], Random.value);
+        return point;
+    }
+}

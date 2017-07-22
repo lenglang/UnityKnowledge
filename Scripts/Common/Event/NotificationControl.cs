01ffd75c7940608037e81af4afb3a281108e0834
王则昆 <364127728@qq.com>
2017-07-22 17:15:36 +0800

    通知消息优化

--- Scripts/Common/Event/NotificationControl.cs
+++ Scripts/Common/Event/NotificationControl.cs
@@ -1,21 +1,22 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+using UnityEngine.Events;
+/// <summary>
+/// 作者-wzk
+/// 功能-消息分发
+/// </summary>
 namespace WZK.Common
 {
     /// <summary>
-    /// 作者-wzk
-    /// 功能-消息分发
-    /// 说明-代码或物体被删除，仍然会执行里面的方法.....，所以添加监听方法的时候，务必在OnDestroy里将监听的方法移除，控制的好，在OnDestroy移除所有RemoveAllEvent
+    /// 通知控制
     /// </summary>
+    /// <typeparam name="T1">枚举类型</typeparam>
     public class NotificationControl<T1>
     {
         /// <summary>
         /// 通知中心单例
         /// </summary>
         private static NotificationControl<T1> _instance = null;
-
         public static NotificationControl<T1> Instance
         {
             get
@@ -23,6 +24,10 @@ namespace WZK.Common
                 if (_instance == null)
                 {
                     _instance = new NotificationControl<T1>();
+                    if (!typeof(T1).IsEnum)
+                    {
+                        throw new ArgumentException("传递的类型请使用枚举");
+                    }
                 }
                 return _instance;
             }
@@ -30,50 +35,42 @@ namespace WZK.Common
         /// <summary>
         /// 存储事件的字典
         /// </summary>
-        private Dictionary<T1, List<Action>> _eventListeners = new Dictionary<T1, List<Action>>();
-
+        private Dictionary<T1, UnityEvent> _eventDictionary = new Dictionary<T1, UnityEvent>();
         /// <summary>
         /// 添加监听事件委托
         /// </summary>
         /// <param name="eventKey">事件Key</param>
         /// <param name="eventListener">事件监听器</param>
-        public void AddEventListener(T1 eventKey, Action action)
+        public void AddEventListener(T1 eventKey, UnityAction action)
         {
-            if (!_eventListeners.ContainsKey(eventKey))
+            if (!_eventDictionary.ContainsKey(eventKey))
             {
-                List<Action> list = new List<Action>();
-                list.Add(action);
-                _eventListeners.Add(eventKey, list);
+                UnityEvent myEvent = new UnityEvent ();
+                myEvent.AddListener(action);
+                _eventDictionary.Add(eventKey,myEvent);
             }
             else
             {
-                if (_eventListeners[eventKey].IndexOf(action) == -1)
-                {
-                    _eventListeners[eventKey].Add(action);
-                }
-                else
-                {
-                    Debug.LogError("重复添加该委托函数");
-                }
+                _eventDictionary[eventKey].AddListener(action);
             }
         }
         /// <summary>
-        /// 移除事件
+        /// 移除某个事件
         /// </summary>
         /// <param name="eventKey">事件Key</param>
         public void RemoveEvent(T1 eventKey)
         {
-            if (!_eventListeners.ContainsKey(eventKey))
+            if (!_eventDictionary.ContainsKey(eventKey))
                 return;
-            _eventListeners[eventKey] = null;
-            _eventListeners.Remove(eventKey);
+            _eventDictionary[eventKey] = null;
+            _eventDictionary.Remove(eventKey);
         }
         /// <summary>
         /// 移除所有事件
         /// </summary>
         public void RemoveAllEvent()
         {
-            _eventListeners.Clear();
+            _eventDictionary.Clear();
             _instance = null;
         }
         /// <summary>
@@ -81,18 +78,11 @@ namespace WZK.Common
         /// </summary>
         /// <param name="eventKey"></param>
         /// <param name="action"></param>
-        public void RemoveEventListener(T1 eventKey, Action action)
+        public void RemoveEventListener(T1 eventKey, UnityAction action)
         {
-            if (!_eventListeners.ContainsKey(eventKey))
+            if (!_eventDictionary.ContainsKey(eventKey))
                 return;
-            for (int i = 0; i < _eventListeners[eventKey].Count; i++)
-            {
-                if (_eventListeners[eventKey][i] == action)
-                {
-                    _eventListeners[eventKey].RemoveAt(i);
-                    break;
-                }
-            }
+            _eventDictionary[eventKey].RemoveListener(action);
         }
         /// <summary>
         /// 发送事件
@@ -100,37 +90,29 @@ namespace WZK.Common
         /// <param name="eventKey"></param>
         public void DispatchEvent(T1 eventKey)
         {
-            if (!_eventListeners.ContainsKey(eventKey))
+            if (!_eventDictionary.ContainsKey(eventKey))
                 return;
-            for (int i = _eventListeners[eventKey].Count - 1; i >= 0; i--)
-            {
-                if (_eventListeners[eventKey][i] == null) Debug.Log("xxxxxxx");
-                try
-                {
-                    _eventListeners[eventKey][i]();
-                }
-                catch (Exception ex)
-                {
-                    Debug.LogError(ex.Message);
-                    _eventListeners[eventKey].RemoveAt(i);
-                }
-            }
+            _eventDictionary[eventKey].Invoke();
         }
         /// <summary>
         /// 是否存在指定事件
         /// </summary>
         public Boolean HasEvent(T1 eventKey)
         {
-            return _eventListeners.ContainsKey(eventKey);
+            return _eventDictionary.ContainsKey(eventKey);
         }
     }
+    /// <summary>
+    /// 通知控制
+    /// </summary>
+    /// <typeparam name="T1">枚举类型</typeparam>
+    /// <typeparam name="T2">传递参数</typeparam>
     public class NotificationControl<T1, T2>
     {
         /// <summary>
         /// 通知中心单例
         /// </summary>
         private static NotificationControl<T1, T2> _instance = null;
-
         public static NotificationControl<T1, T2> Instance
         {
             get
@@ -138,56 +120,49 @@ namespace WZK.Common
                 if (_instance == null)
                 {
                     _instance = new NotificationControl<T1, T2>();
+                    if (!typeof(T1).IsEnum)
+                    {
+                        throw new ArgumentException("传递的类型请使用枚举");
+                    }
                 }
                 return _instance;
             }
         }
-        public delegate void ReceiveData(T2 data);
-
+        public class MyEvent : UnityEvent<T2> { }
         /// <summary>
         /// 存储事件的字典
         /// </summary>
-        private Dictionary<T1, List<ReceiveData>> _eventListeners = new Dictionary<T1, List<ReceiveData>>();
-
-        public void AddEventListener(T1 eventKey, ReceiveData receiveData)
+        private Dictionary<T1, MyEvent> _eventDictionary = new Dictionary<T1, MyEvent>();
+        public void AddEventListener(T1 eventKey, UnityAction<T2> action)
         {
-            if (!_eventListeners.ContainsKey(eventKey))
+            if (!_eventDictionary.ContainsKey(eventKey))
             {
-                List<ReceiveData> list = new List<ReceiveData>();
-                list.Add(receiveData);
-                _eventListeners.Add(eventKey, list);
+                MyEvent myEvent = new MyEvent();
+                myEvent.AddListener(action);
+                _eventDictionary.Add(eventKey, myEvent);
             }
             else
             {
-                if (_eventListeners[eventKey].IndexOf(receiveData) == -1)
-                {
-                    _eventListeners[eventKey].Add(receiveData);
-                }
-                else
-                {
-                    Debug.LogError("重复添加该委托函数");
-                }
+                _eventDictionary[eventKey].AddListener(action);
             }
         }
-
         /// <summary>
-        /// 移除事件
+        /// 移除某个事件
         /// </summary>
         /// <param name="eventKey">事件Key</param>
         public void RemoveEvent(T1 eventKey)
         {
-            if (!_eventListeners.ContainsKey(eventKey))
+            if (!_eventDictionary.ContainsKey(eventKey))
                 return;
-            _eventListeners[eventKey] = null;
-            _eventListeners.Remove(eventKey);
+            _eventDictionary[eventKey] = null;
+            _eventDictionary.Remove(eventKey);
         }
-
         /// <summary>
         /// 移除所有事件
         /// </summary>
         public void RemoveAllEvent()
         {
-            _eventListeners.Clear();
+            _eventDictionary.Clear();
             _instance = null;
         }
         /// <summary>
@@ -195,49 +170,29 @@ namespace WZK.Common
         /// </summary>
         /// <param name="eventKey"></param>
         /// <param name="sendData"></param>
-        public void RemoveEventListener(T1 eventKey, ReceiveData receiveData)
+        public void RemoveEventListener(T1 eventKey, UnityAction<T2> action)
         {
-            if (!_eventListeners.ContainsKey(eventKey))
+            if (!_eventDictionary.ContainsKey(eventKey))
                 return;
-            for (int i = 0; i < _eventListeners[eventKey].Count; i++)
-            {
-                if (_eventListeners[eventKey][i] == receiveData)
-                {
-                    _eventListeners[eventKey].RemoveAt(i);
-                    break;
-                }
-            }
+            _eventDictionary[eventKey].RemoveListener(action);
         }
-
         /// <summary>
         /// 发送事件
         /// </summary>
         /// <param name="eventKey"></param>
         /// <param name="receiveData"></param>
-        public void DispatchEvent(T1 eventKey, T2 receiveData)
+        public void DispatchEvent(T1 eventKey, T2 data)
         {
-            if (!_eventListeners.ContainsKey(eventKey))
+            if (!_eventDictionary.ContainsKey(eventKey))
                 return;
-            for (int i = _eventListeners[eventKey].Count - 1; i >= 0; i--)
-            {
-                try
-                {
-                    _eventListeners[eventKey][i](receiveData);
-                }
-                catch (Exception ex)
-                {
-                    Debug.LogError(ex.Message);
-                    _eventListeners[eventKey].RemoveAt(i);
-                }
-            }
+            _eventDictionary[eventKey].Invoke(data);
         }
-
         /// <summary>
         /// 是否存在指定事件
         /// </summary>
         public Boolean HasEvent(T1 eventKey)
         {
-            return _eventListeners.ContainsKey(eventKey);
+            return _eventDictionary.ContainsKey(eventKey);
         }
     }
 }
--- Scripts/Common/Event/NotificationExample.cs
+++ Scripts/Common/Event/NotificationExample.cs
@@ -3,7 +3,6 @@ namespace WZK.Common
 {
     public class NotificationExample : MonoBehaviour
     {
-
         public enum EventType
         {
             学生
@@ -11,52 +10,26 @@ namespace WZK.Common
         // Use this for initialization
         void Start()
         {
-            NotificationControl<int>.Instance.AddEventListener(1, OnComplete5);
-            NotificationControl<int>.Instance.AddEventListener(1, OnComplete5);
-            NotificationControl<int>.Instance.AddEventListener(1, OnComplete5);
-            NotificationControl<string>.Instance.AddEventListener("2", OnComplete5);
-            NotificationControl<EventType>.Instance.AddEventListener(EventType.学生, OnComplete5);
-
-            NotificationControl<int>.Instance.DispatchEvent(1);
-            NotificationControl<string>.Instance.DispatchEvent("2");
+            NotificationControl<EventType>.Instance.AddEventListener(EventType.学生, delegate { Debug.Log("666"); });
+            NotificationControl<EventType>.Instance.AddEventListener(EventType.学生, OnComplete1);
             NotificationControl<EventType>.Instance.DispatchEvent(EventType.学生);
 
-            NotificationControl<int, int>.Instance.AddEventListener(1, OnComplete2);
-            NotificationControl<string, string>.Instance.AddEventListener("2", OnComplete3);
-            NotificationControl<EventType, NotificationContent>.Instance.AddEventListener(EventType.学生, OnComplete4);
-            NotificationControl<EventType, NotificationContent>.Instance.AddEventListener(EventType.学生, OnComplete4);
-
-            NotificationControl<int, int>.Instance.DispatchEvent(1, 1);
-            NotificationControl<string, string>.Instance.DispatchEvent("2", "2");
+            NotificationControl<EventType, NotificationContent>.Instance.AddEventListener(EventType.学生, OnComplete2);
             NotificationContent nc = new NotificationContent();
             NotificationControl<EventType, NotificationContent>.Instance.DispatchEvent(EventType.学生, nc);
         }
 
-        private void OnComplete5()
+        private void OnComplete1()
         {
             Debug.Log("通知方法1");
         }
-
-        private void OnComplete2(int data)
-        {
-            Debug.Log("通知方法2:" + data);
-        }
-
-        private void OnComplete3(string data)
-        {
-            Debug.Log("通知方法2:" + data);
-        }
-
-        private void OnComplete4(NotificationContent data)
+        private void OnComplete2(NotificationContent data)
         {
             Debug.Log("通知方法2:" + data._name.Contains("王"));
         }
         private void OnDestroy()
         {
-            //事件移除
-            NotificationControl<int>.Instance.RemoveEvent(1);
-            NotificationControl<string>.Instance.RemoveEvent("2");
-            //不可使用NotificationControl<string>.Instance.RemoveAllEvent();
+            //内部使用最好将事件移除
             NotificationControl<EventType>.Instance.RemoveAllEvent();
         }
     }

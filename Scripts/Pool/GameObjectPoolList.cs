da5c3c3e4fe1850fed4b4c71db37b0a1f4d9480c
王则昆 <364127728@qq.com>
2017-07-25 16:49:22 +0800

    对象池创建菜单，名字更改

--- Scripts/Pool/GameObjectPoolList.cs
+++ Scripts/Pool/GameObjectPoolList.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-[CreateAssetMenu(fileName ="文件名",menuName = "Test/GameObjectPoolList",order =0)]
+[CreateAssetMenu(fileName ="对象池",menuName = "创建对象池",order =1000)]
 public class GameObjectPoolList : ScriptableObject
 {//继承自ScriptableObject 表示吧类GameObjectPoolList变成可以自定义资源配置的文件
     public List<GameObjectPool> poolList;

70db36524e1e00382409b2d7a105ceaea41709f2
王则昆 <364127728@qq.com>
2017-07-28 13:54:34 +0800

    忽略文件夹

--- _Common/Scripts/Global/Tools/AudioClipHelper.cs
+++ _Common/Scripts/Global/Tools/AudioClipHelper.cs
@@ -1,69 +1,66 @@
 ﻿using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
-namespace WZK.Common
+public class AudioClipHelper
 {
-    public class AudioClipHelper
+    public static AudioClip Combine(params AudioClip[] clips)
     {
-        public static AudioClip Combine(params AudioClip[] clips)
+        if (clips == null || clips.Length == 0)
+            return null;
+        int channels = clips[0].channels;
+        int frequency = clips[0].frequency;
+        for (int i = 1; i < clips.Length; i++)
         {
-            if (clips == null || clips.Length == 0)
+            if (clips[i].channels != channels || clips[i].frequency != frequency)
+            {
                 return null;
-            int channels = clips[0].channels;
-            int frequency = clips[0].frequency;
-            for (int i = 1; i < clips.Length; i++)
-            {
-                if (clips[i].channels != channels || clips[i].frequency != frequency)
-                {
-                    return null;
-                }
-            }
-            using (MemoryStream memoryStream = new MemoryStream())
-            {
-                for (int i = 0; i < clips.Length; i++)
-                {
-                    if (clips[i] == null)
-                        continue;
-
-                    clips[i].LoadAudioData();
-                    var buffer = clips[i].GetData();
-                    memoryStream.Write(buffer, 0, buffer.Length);
-                }
-                var bytes = memoryStream.ToArray();
-                var result = AudioClip.Create("Combine", bytes.Length / 4 / channels, channels, frequency, false);
-                result.SetData(bytes);
-                return result;
             }
         }
-        public static AudioClip Combine(List<AudioClip> clips)
+        using (MemoryStream memoryStream = new MemoryStream())
         {
-            if (clips == null || clips.Count == 0)
-                return null;
-            int channels = clips[0].channels;
-            int frequency = clips[0].frequency;
-            for (int i = 1; i < clips.Count; i++)
+            for (int i = 0; i < clips.Length; i++)
             {
-                if (clips[i].channels != channels || clips[i].frequency != frequency)
-                {
-                    return null;
-                }
-            }
-            using (MemoryStream memoryStream = new MemoryStream())
-            {
-                for (int i = 0; i < clips.Count; i++)
-                {
-                    if (clips[i] == null)
-                        continue;
+                if (clips[i] == null)
+                    continue;
 
-                    clips[i].LoadAudioData();
-                    var buffer = clips[i].GetData();
-                    memoryStream.Write(buffer, 0, buffer.Length);
-                }
-                var bytes = memoryStream.ToArray();
-                var result = AudioClip.Create("Combine", bytes.Length / 4 / channels, channels, frequency, false);
-                result.SetData(bytes);
-                return result;
+                clips[i].LoadAudioData();
+                var buffer = clips[i].GetData();
+                memoryStream.Write(buffer, 0, buffer.Length);
             }
+            var bytes = memoryStream.ToArray();
+            var result = AudioClip.Create("Combine", bytes.Length / 4 / channels, channels, frequency, false);
+            result.SetData(bytes);
+            return result;
+        }
+    }
+    public static AudioClip Combine(List<AudioClip> clips)
+    {
+        if (clips == null || clips.Count == 0)
+            return null;
+        int channels = clips[0].channels;
+        int frequency = clips[0].frequency;
+        for (int i = 1; i < clips.Count; i++)
+        {
+            if (clips[i].channels != channels || clips[i].frequency != frequency)
+            {
+                return null;
+            }
+        }
+        using (MemoryStream memoryStream = new MemoryStream())
+        {
+            for (int i = 0; i < clips.Count; i++)
+            {
+                if (clips[i] == null)
+                    continue;
+
+                clips[i].LoadAudioData();
+                var buffer = clips[i].GetData();
+                memoryStream.Write(buffer, 0, buffer.Length);
+            }
+            var bytes = memoryStream.ToArray();
+            var result = AudioClip.Create("Combine", bytes.Length / 4 / channels, channels, frequency, false);
+            result.SetData(bytes);
+            return result;
         }
     }
 }

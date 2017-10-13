029620b47acaf4b9a53986e2783a7418a6ff225f
王则昆 <364127728@qq.com>
2017-10-13 09:33:56 +0800

    ScrollRect优化

---
+++ Scripts/Tool/ScrollRectEventListener.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+namespace WZK
+{
+    public class ScrollRectEventListener : MonoBehaviour, IPointerClickHandler, IPointerDownHandler, IPointerEnterHandler, IPointerExitHandler, IPointerUpHandler
+    {
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            Debug.Log("点击了" + name);
+        }
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            Debug.Log("按下了" + name);
+        }
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            
+        }
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            
+        }
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            
+        }
+    }
+}
